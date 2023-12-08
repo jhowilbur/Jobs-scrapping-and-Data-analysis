45cdf2b600adf493877f3f814e8041791737b68d
Wilbur <wilbur@wilbur.com>
2023-12-08 17:21:05 -0500

    -refactoring

--- linkedin_job_scraper.py
+++ linkedin_job_scraper.py
@@ -1,6 +1,6 @@
 import csv
 import random
-from time import sleep
+import time
 
 import requests
 from bs4 import BeautifulSoup
@@ -33,98 +33,97 @@ links = {
 }
 
 
-# Function to scrap job listings
-def create_job_csv(country_links: dict, country: str):
-    with open('datasets/linkedin-jobs-' + country + '.csv', mode='w', encoding='UTF-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(['title', 'company', 'description', 'onsite_remote',
-                         'salary', 'location', 'criteria', 'posted_date', 'link'])
-
-        # Moving from page to page
-        def linkedin_scraper(webpage, page_number, onsite_remote):
-            count = 0
-            next_page = webpage + str(page_number)
-            response = requests.get(str(next_page))
-            soup = BeautifulSoup(response.content, 'html.parser')
-            # print(response.content)
-            jobs = soup.find_all(
-                'div', class_='base-card relative w-full hover:no-underline focus:no-underline base-card--link '
-                              'base-search-card base-search-card--link job-search-card')
-            for job in jobs:
-                job_criteria = []
-                job_title = job.find(
-                    'h3', class_='base-search-card__title').text.strip()
-                job_company = job.find(
-                    'h4', class_='base-search-card__subtitle').text.strip()
-                job_location = job.find(
-                    'span', class_='job-search-card__location').text.strip()
-                job_datetime = job.find(
-                    'time', class_='job-search-card__listdate')['datetime'] if job.find(
-                    'time', class_='job-search-card__listdate') is not None else job.find(
-                    'time', class_='job-search-card__listdate--new')['datetime']
-                job_salary = job.find('span', class_='job-search-card__salary-info').text.strip(
-                ) if job.find('span', class_='job-search-card__salary-info') is not None else "NaN"
-
-                job_link = job.find('a', class_='base-card__full-link')['href']
-
-                while True:
-                    try:
-                        sleep(random.randint(1, 5))
-                        resp = requests.get(job_link)
-                        sp = BeautifulSoup(resp.content, 'html.parser')
-
-                        # Save requests as html pages to help view classes for scraping
-                        if count == 0 and country == 'africa':
-                            with open('scrap_page_view/job-list.html', mode='w', encoding="utf-8") as job_list:
-                                job_list.write(str(response.content))
-                                job_list.close()
-                            with open('scrap_page_view/job.html', mode='w', encoding="utf-8") as job_detail:
-                                job_detail.write(str(resp.content))
-                                job_detail.close()
-                        count += 1
-
-                        job_desc = sp.find('div',
-                                           class_='show-more-less-html__markup '
-                                                  'show-more-less-html__markup--clamp-after-5').text.strip(
-                        ) if sp.find('div',
-                                     class_='show-more-less-html__markup show-more-less-html__markup--clamp-after-5') is not None else "Nan"
-
-                        criteria = sp.find_all(
-                            'li', class_='description__job-criteria-item')
-                        for criterion in criteria:
-                            feature = criterion.find(
-                                'h3', class_='description__job-criteria-subheader').text.strip()
-                            value = criterion.find(
-                                'span',
-                                class_='description__job-criteria-text description__job-criteria-text--criteria').text.strip()
-                            job_criteria.append({feature: value})
-
-                        writer.writerow([job_title, job_company, job_desc, onsite_remote, job_salary,
-                                         job_location, job_criteria, job_datetime, job_link])
-                        print('Data updated')
-                        break
-
-                    except:
-                        print("Connection refused by the server..")
-                        sleep(5)
-                        continue
-
-            if page_number < 500:
-                # Move to the next page
-                page_number = page_number + 25
-                linkedin_scraper(webpage, page_number, onsite_remote)
-
-        for work_type in country_links:
-            linkedin_scraper(country_links[work_type], 0, work_type)
-
-
-for country in links:
-
-    while True:
+def fetch_page_content(url, max_retries=5):
+    """Fetch and return page content for a given URL, with retries and delays."""
+    retries = 0
+    while retries < max_retries:
         try:
-            create_job_csv(links[country], country)
-            break
-        except:
-            print("Connection refused by the server..")
-            sleep(5)
-            continue
+            response = requests.get(url)
+            response.raise_for_status()
+            return response.content
+        except requests.RequestException as e:
+            wait = 2 ** retries  # Exponential backoff
+            print(f"Error fetching URL {url}: {e}. Retrying in {wait} seconds.")
+            time.sleep(wait + random.uniform(0, 1))  # Adding randomness to the wait time
+            retries += 1
+    print(f"Failed to fetch {url} after {max_retries} retries.")
+    return None
+
+
+def parse_job_listing(job):
+    """Parse and return job listing details."""
+    try:
+        job_title = job.find('h3', class_='base-search-card__title').text.strip()
+        job_company = job.find('h4', class_='base-search-card__subtitle').text.strip()
+        job_location = job.find('span', class_='job-search-card__location').text.strip()
+
+        job_datetime = job.find('time', class_='job-search-card__listdate')
+        job_datetime = job_datetime['datetime'] if job_datetime else 'Unknown'
+
+        job_salary = job.find('span', class_='job-search-card__salary-info')
+        job_salary = job_salary.text.strip() if job_salary else 'N/A'
+
+        job_link = job.find('a', class_='base-card__full-link')['href']
+
+        # Fetching job description and criteria from the job detail page
+        job_detail_content = fetch_page_content(job_link)
+        if job_detail_content:
+            soup_detail = BeautifulSoup(job_detail_content, 'html.parser')
+            job_desc = soup_detail.find('div', class_='show-more-less-html__markup')
+            job_desc = job_desc.text.strip() if job_desc else 'N/A'
+
+            criteria_elements = soup_detail.find_all('li', class_='description__job-criteria-item')
+            job_criteria = []
+            for criterion in criteria_elements:
+                feature = criterion.find('h3', class_='description__job-criteria-subheader').text.strip()
+                value = criterion.find('span', class_='description__job-criteria-text').text.strip()
+                job_criteria.append(f"{feature}: {value}")
+
+            return job_title, job_company, job_desc, job_salary, job_location, job_criteria, job_datetime, job_link
+
+    except Exception as e:
+        print(f"Error parsing job listing: {e}")
+        return None
+
+
+def save_page_for_debugging(country, content, file_name):
+    """Save HTML content to a file for debugging."""
+    if country == 'africa':
+        with open(f'scrap_page_view/{file_name}.html', 'w', encoding='utf-8') as file:
+            file.write(str(content))
+
+
+def scrape_jobs(writer, country, webpage, work_type):
+    """Scrape job listings and write to CSV."""
+    page_number = 0
+    while page_number < 500:
+        next_page = f"{webpage}{page_number}"
+        page_content = fetch_page_content(next_page)
+
+        if page_content:
+            soup = BeautifulSoup(page_content, 'html.parser')
+            jobs = soup.find_all('div', class_='base-card')  # Update class name if necessary
+
+            for job in jobs:
+                job_details = parse_job_listing(job)
+                if job_details:
+                    writer.writerow([*job_details, work_type])
+
+            save_page_for_debugging(country, page_content, 'job-list')
+            page_number += 25
+
+
+def create_job_csv(country_links, country):
+    """Create CSV file for job listings of a country."""
+    file_path = f'datasets/linkedin-jobs-{country}.csv'
+    with open(file_path, mode='w', newline='', encoding='UTF-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            ['title', 'company', 'description', 'work_type', 'salary', 'location', 'criteria', 'posted_date', 'link'])
+
+        for work_type, link in country_links.items():
+            scrape_jobs(writer, country, link, work_type)
+
+
+for country, links in links.items():
+    create_job_csv(links, country)
